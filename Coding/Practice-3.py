dcbd3bda4f3af305eaecfafbe4c08b2a680f4844
imm-rl-lab <a.r.plaksin@gmail.com>
2020-10-29 17:57:59 +0500

    Update Practice-3.py

--- Coding/Practice-3.py
+++ Coding/Practice-3.py
@@ -1,96 +1,120 @@
-from Frozen_Lake import FrozenLakeEnv
-env = FrozenLakeEnv()
-
-# print(env.get_all_states())
-# state = (3, 2)
-# print(env.get_possible_actions(state))
-# action = 'right'
-# print(env.get_next_states(state, action))
-# next_state
-# = (3, 3)
-# print(env.get_reward(state, action, next_state))
-# print(env.get_transition_prob(state, action, next_state))
-# print(env.is_terminal(next_state))
-
 import numpy as np
+from Frozen_Lake import FrozenLakeEnv
 
-GAMMA = 0.99
 
-def test(pi, test_n=1000, step_n=100):
-    rewards = np.zeros(test_n)
-    for i in range(test_n):
-        s = env.reset()
-        for t in range(step_n):
-            #env.render()
-            prob = [pi[s][a] for a in env.get_possible_actions(s)]
-            a = np.random.choice(env.get_possible_actions(s), p=prob)
-            next_s, reward, done, _ = env.step(a)
-            rewards[i] += reward# * GAMMA ** t
-            s = next_s
-            if done:
-                break
-    return np.mean(rewards)
+def init_policy(env):
+    policy = {}
+    for state in env.get_all_states():
+        policy[state] = {}
+        for action in env.get_possible_actions(state):
+            policy[state][action] = 1 / len(env.get_possible_actions(state))
+    return policy
 
-def get_q(v):
-    q = {}
-    for s in env.get_all_states():
-        q[s] = {}
-        for a in env.get_possible_actions(s):
-            q[s][a] = 0
-            for next_s in env.get_next_states(s, a):
-                q[s][a] += env.get_transition_prob(s, a, next_s) * \
-                           (env.get_reward(s, a, next_s) + GAMMA * v[next_s])
-    return q
 
-def policy_evaluation(pi, iteration_n=100):
-    v = {s : 0 for s in env.get_all_states()}
-    for _ in range(iteration_n):
-        q = get_q(v)
-        new_v = {}
-        for s in env.get_all_states():
-            new_v[s] = 0
-            for a in env.get_possible_actions(s):
-                new_v[s] += pi[s][a] * q[s][a]
-        v = new_v
-    return v
+def init_values(env):
+    values = {}
+    for state in env.get_all_states():
+        values[state] = 0
+    return values
 
-def policy_improvment(v):
-    q = get_q(v)
-    pi = {}
-    for s in env.get_all_states():
-        pi[s] = {}
-        if len(env.get_possible_actions(s)) != 0:
-            q_max = max([q[s][a] for a in env.get_possible_actions(s)])
+
+def init_q_values():
+    q_values = {}
+    for state in env.get_all_states():
+        q_values[state] = {}
+        for action in env.get_possible_actions(state):
+            q_values[state][action] = 0
+    return q_values
+
+
+def get_q_values(env, gamma, values):
+    q_values = init_q_values()
+    for state in env.get_all_states():
+        for action in env.get_possible_actions(state):
+            for next_state in env.get_next_states(state, action):
+                q_values[state][action] += env.get_reward(
+                    state, action, next_state) + gamma * env.get_transition_prob(
+                    state, action, next_state) * values[next_state]
+    return q_values
+
+
+def update_values(env, gamma, values, policy):
+    new_values = init_values(env)
+    for state in env.get_all_states():
+        for action in env.get_possible_actions(state):
+            q_values = get_q_values(env, gamma, values)
+            new_values[state] += policy[state][action] * q_values[state][action]
+    return new_values
+
+
+def policy_evaluation(env, gamma, policy, M):
+    values = init_values(env)
+    for _ in range(M):
+        values = update_values(env, gamma, values, policy)
+    return values
+
+
+def policy_improvement(env, gamma, values):
+    q_values = get_q_values(env, gamma, values)
+    policy = init_policy(env)
+    for state in env.get_all_states():
+        if len(env.get_possible_actions(state)) > 0:
+            max_q_value = max([q_values[state][action] for action in env.get_possible_actions(state)])
             there_was_max = False
-            for a in env.get_possible_actions(s):
-                if q[s][a] == q_max and not there_was_max:
-                    pi[s][a] = 1
+            for action in env.get_possible_actions(state):
+                if q_values[state][action] == max_q_value and not there_was_max:
+                    policy[state][action] = 1
                     there_was_max = True
                 else:
-                    pi[s][a] = 0
-    return pi
+                    policy[state][action] = 0
+    return policy
 
-def policy_iteration(iteration_n=20):
-    pi = {s : {a : 1/4 for a in env.get_possible_actions(s)} for s in env.get_all_states()}
-    for i in range(iteration_n):
-        v = policy_evaluation(pi)
-        pi = policy_improvment(v)
-    return pi
 
-def value_iteration(iteration_n=20):
-    v = {s: 0 for s in env.get_all_states()}
-    for _ in range(iteration_n):
-        q = get_q(v)
-        for s in env.get_all_states():
-            if len(env.get_possible_actions(s)) != 0:
-                v[s] = max(q[s][a] for a in env.get_possible_actions(s))
-    return policy_improvment(v)
+def policy_iteration(env, gamma, N=20, M=20):
+    policy = init_policy(env)
+    for _ in range(N):
+        values = policy_evaluation(env, gamma, policy, M)
+        policy = policy_improvement(env, gamma, values)
+    return policy
 
-pi = policy_iteration()
-print(pi)
-print(test(pi))
 
-pi = value_iteration()
-print(pi)
-print(test(pi))
+def get_total_reward(env, policy, session_len):
+    total_reward = 0
+    state = env.reset()
+    for _ in range(session_len):
+        prob = [policy[state][action] for action in env.get_possible_actions(state)]
+        action = np.random.choice(env.get_possible_actions(state), p=prob)
+        state, reward, done, _ = env.step(action)
+        total_reward += reward
+        if done:
+            break
+    return total_reward
+
+
+def policy_test(env, policy, session_n, session_len=100):
+    total_rewards = np.array([get_total_reward(env, policy, session_len) for _ in range(session_n)])
+    return np.mean(total_rewards)
+
+
+def value_iteration(env, gamma, N=20):
+    values = init_values(env)
+    for _ in range(N):
+        q_values = get_q_values(env, gamma, values)
+        for state in env.get_all_states():
+            if len(env.get_possible_actions(state)) > 0:
+                values[state] = max(q_values[state][action] for action in env.get_possible_actions(state))
+
+    policy = policy_improvement(env, gamma, values)
+    return policy
+
+
+env = FrozenLakeEnv()
+gamma = 0.99
+
+policy = value_iteration(env, gamma, N=500)
+print('value_iteration:', policy_test(env, policy, session_n=500))
+
+policy = policy_iteration(env, gamma, N=20, M=20)
+print('policy_iteration:', policy_test(env, policy, session_n=500))
+
 
